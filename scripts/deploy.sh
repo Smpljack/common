64d5e31a03cacc0d40899335217405c594c9f624
Tobias Kölling <tobi@die70.de>
2016-12-08 14:18:34 +0100

    added debug messages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,11 @@ export BASEDIR="${SCRIPTDIR}/.."
 export GENERATORDIR="${BASEDIR}/page_generator"
 export OUTPUTDIR="${BASEDIR}/docs/generated"
 
+echo "generating pages..."
 ${GENERATORDIR}/generate.sh
+echo "showing changes..."
+git diff "${OUTPUTDIR}"
+echo "deploying..."
 git add "${OUTPUTDIR}"
 ${SCRIPTDIR}/commit.sh
 ${SCRIPTDIR}/travis_push.sh
